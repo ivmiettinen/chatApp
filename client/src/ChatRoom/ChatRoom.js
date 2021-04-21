1028f3fcdf963e867b4959d6520728584157366c
Ilkka <55871738+ivmiettinen@users.noreply.github.com>
2021-04-21 14:24:37 +0300

    first websocket push, join room and writing messages available

---
+++ client/src/ChatRoom/ChatRoom.js
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+import './ChatRoom.css'
+import UseChat from './UseChat'
+
+const ChatRoom = (props) => {
+    const { roomId } = props.match.params // Gets roomId from URL
+    const { messages, sendMessage } = UseChat(roomId) // Creates a websocket and manages messaging
+    const [newMessage, setNewMessage] = useState('') // Message to be sent
+
+    const handleNewMessageChange = (event) => {
+        setNewMessage(event.target.value)
+    }
+
+    const handleSendMessage = () => {
+        sendMessage(newMessage)
+        setNewMessage('')
+    }
+
+    return (
+        <div className='chat-room-container'>
+            <h1 className='room-name'>Room: {roomId}</h1>
+            <div className='messages-container'>
+                <ol className='messages-list'>
+                    {messages.map((message, i) => (
+                        <li
+                            key={i}
+                            className={`message-item ${
+                                message.ownedByCurrentUser
+                                    ? 'my-message'
+                                    : 'received-message'
+                            }`}
+                        >
+                            {message.body}
+                        </li>
+                    ))}
+                </ol>
+            </div>
+            <textarea
+                value={newMessage}
+                onChange={handleNewMessageChange}
+                placeholder='Write message...'
+                className='new-message-input-field'
+            />
+            <button onClick={handleSendMessage} className='send-message-button'>
+                Send
+            </button>
+        </div>
+    )
+}
+
+export default ChatRoom
